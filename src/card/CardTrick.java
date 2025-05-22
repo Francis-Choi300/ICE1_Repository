ded7e6b8db488ffe07e757ae9795d6447963669d
Francis-Choi300 <chyoonho@sheridancollege.ca>
2025-05-22 00:43:54 -0400

    Update CardTrick.java
    Removed pick a Card,
    Added luckyCard

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -38,71 +38,11 @@ public class CardTrick {
             //print the values of the card
             System.out.println( i+1 + "th card \n" +c.toString() + "\n");
         }
-        
-        
-        //insert code to ask the user for Card value and suit, create their card
-        final String [] suitList = {"Hearts", "Diamonds", "Spades", "Clubs"};
-        
-        
-        //create scanner and create a card
-        Card pickCard = new Card();
-        Scanner scan = new Scanner(System.in);
-        
-        
-        //a loop continues until both Suit and Value are valid 
-        while(pickCard.getSuit()==null || pickCard.getValue()==0){
-        //ask user for a number 1-13, encase in try block incase of misinput
-        try{
-        System.out.println("Enter a Number between 1-13: ");
-        String input = scan.nextLine();
-        
-        //Parse the input to see if its an Integer
-        int number = Integer.parseInt(input);
-        
-        //if number between 1-13, set number of card
-        //if not throw out of bounds Exception
-        if (number >= 1 && number <= 13)
-            {
-            System.out.println("Number Selected: " + number + "\n");
-            pickCard.setValue(number);
-            }
-        else
-            {throw new Exception();}
-        }//end try block
-        catch(Exception err)
-           {System.out.println("Invalid Input, try again\n");}
-        
-        
-        //ask user for a suit, encase in try block incase of misinput
-        try{
-        //ask user for a suit
-        System.out.println("Enter a Card Suit:");
-        String input1 = scan.nextLine();
-        
-        //Loop through the list of suits
-        
-        for (int i = 0; i <= suitList.length ; i++){
-            
-            // if the input matches a valid suit then set the suit and break the for loop
-            // if the input does not match a suit the for loop will go out of bounds and cause an exception
-            if (suitList[i].equals(input1))
-                   {pickCard.setSuit(input1);
-                    System.out.println("Suit Selected: " + input1 + "\n");
-                    break;}
 
-            }//end for loop
-        
-
-        }//end try block
-
-        catch(Exception e){
-        System.out.println("Invalid Input, try again\n");
-        }
-        
-        }//end while loop
-        
-        //finally you can print the chosen card
-        
+        // add one luckcard hard code 2,clubs
+        Card luckyCard = new Card();
+            luckyCard.setSuit("Spades");
+            luckyCard.setValue(1);
         System.out.println(pickCard.toString());
     
         // and search magicHand here
@@ -111,12 +51,12 @@ public class CardTrick {
         String result = "No Match"; 
         
         for (Card card : magicHand){
-            if (card.toString().equals(pickCard.toString()))
+            if (card.toString().equals(luckyCard.toString()))
                 { result = "Match";}
         }
         System.out.println("Result: " + result);
         
-        // add one luckcard hard code 2,clubs
+
     }
     
 }
